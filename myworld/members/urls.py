587347f216e9107c7ee999a67e218dd9fd181105
adithyan-777 <adithyan92014@gmail.com>
2022-05-22 19:56:36 +0530

    added new line at the end

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('delete/<int:id>', views.delete, name='delete'),
     path('update/<int:id>', views.update, name='update'),
     path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
-]   
+]
